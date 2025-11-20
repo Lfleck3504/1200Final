5c143dbab9ceafcc4f2408a0382da007ba88ec9c
Lfleck3504 <lfleck3504@gmail.com>
2025-11-20 12:18:14 -0500

    Add files via upload

---
+++ 1200_Final.py
@@ -0,0 +1,10 @@
+#Function to take Product Name(s) as a list
+
+#Function to take Product Price as a float (allow decimal NO INT)
+
+#Function to take Product quantity as an INT
+
+#Function to List Product Name with its appropriate price and quantity
+
+#Make it look pretty (Optional bonus)
+
