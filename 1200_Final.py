dbe30ed078be91d261844af4df6b60f4b1148057
Catman15207 <jromero4@lions.molloy.edu>
2025-12-08 20:44:40 -0500

    1200_Final.py
    I added the price input function, which prompts the user to enter a float, and then all those numbers are saved into a list.  After the user types "quit", it should display all the user's input prices vertically in the order they were entered.

--- 1200_Final.py
+++ 1200_Final.py
@@ -1,10 +1,24 @@
 #Function to take Product Name(s) as a list
 
 #Function to take Product Price as a float (allow decimal NO INT)
+def prices():
+    price = []
+    print('Type the word "quit" when you are done')
+    while True:
+        userinput = input("Enter price: ")
+        if userinput.lower() == "quit":
+            break
 
+        price.append(float(userinput))
+    print("price listed:")
+    for p in price:
+        print(p)
+
+prices()
 #Function to take Product quantity as an INT
 
 #Function to List Product Name with its appropriate price and quantity
 
 #Make it look pretty (Optional bonus)
+
 
